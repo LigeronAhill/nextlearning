877d57e18c190a701595aeca01f2df29d9bf2e3e
Aleksandr Provotorov <provotorov@inbox.ru>
2024-09-13 14:48:21 +0300

    userbutton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,16 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
