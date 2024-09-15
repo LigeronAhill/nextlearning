7cc54c7aac929ae3c4475f767e1f0f9c14d0d15d
Aleksandr Provotorov <provotorov@inbox.ru>
2024-09-15 23:30:06 +0300

    questions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,83 @@
+import QuestionCard, { Question } from "@/components/cards/QuestionCard";
 import Homefilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+const questions: Question[] = [
+  {
+    _id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      {
+        _id: 1,
+        name: "zig",
+      },
+      {
+        _id: 2,
+        name: "sql",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "Rob Pike",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 300000,
+    views: 1000000,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      {
+        _id: 1,
+        name: "zig",
+      },
+      {
+        _id: 2,
+        name: "sql",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "Rob Pike",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: 1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      {
+        _id: 1,
+        name: "zig",
+      },
+      {
+        _id: 2,
+        name: "sql",
+      },
+    ],
+    author: {
+      _id: 1,
+      name: "Rob Pike",
+      picture: "/assets/icons/avatar.svg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+];
 
 const Home = () => {
   return (
@@ -32,6 +105,18 @@ const Home = () => {
         />
       </div>
       <Homefilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((q) => <QuestionCard key={q._id} q={q} />)
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+export interface Question {
+  _id: number;
+  title: string;
+  tags: Tag[];
+  author: Author;
+  upvotes: number;
+  views: number;
+  answers: Answer[];
+  createdAt: Date;
+}
+export interface Tag {
+  _id: number;
+  name: string;
+}
+export interface Author {
+  _id: number;
+  name: string;
+  picture: string;
+}
+export interface Answer {
+  a: string;
+}
+const QuestionCard = ({ q }: { q: Question }) => {
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5  sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(q.createdAt)}
+          </span>
+          <Link href={`/question/${q._id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {q.title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {q.tags.map((t) => (
+          <RenderTag key={t._id} _id={t._id} name={t.name} />
+        ))}
+      </div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl={q.author.picture}
+          alt="User"
+          value={q.author.name}
+          title={` - asked ${getTimestamp(q.createdAt)}`}
+          href={`/profile/${q.author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(q.upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(q.answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(q.views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
