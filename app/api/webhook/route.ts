b860e0fe4c07b956a8b14beb8148044794e6f80c
Aleksandr Provotorov <provotorov@inbox.ru>
2024-09-19 20:19:34 +0300

    rm some

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -54,8 +54,6 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   // const { id } = evt.data;
   const eventType = evt.type;
-  // console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  // console.log("Webhook body:", body);
   switch (eventType) {
     case "user.created": {
       const {
